9a0a9e36b384689571592e21d0d3fadaaf0f659c
tkriszt <theo@kriszt.fr>
2018-12-20 18:06:02 +0100

    Essai fix package dev only (pour deploy sur Heroku)

--- src/AppBundle/DataFixtures/ArticleFixtures.php
+++ src/AppBundle/DataFixtures/ArticleFixtures.php
@@ -29,7 +29,7 @@ class ArticleFixtures extends Fixture
         for ($i = 0; $i < 20; $i++) {
             $article = new Article();
 
-            $article->setTitre('article '.$i);
+            $article->setTitle('article '.$i);
             $article->setContent('Lorem ipsum dolor sit amet et consecuetir');
             $date = mt_rand(1, 28) . '-' . mt_rand(1, 12) . '-' . mt_rand(2013, 2018);
             $article->setPublished( new \DateTime($date) );
---
+++ src/AppBundle/DataFixtures/UsersFixtures.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace AppBundle\DataFixtures;
+
+use AppBundle\Entity\Article;
+use AppBundle\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+
+/**
+ *
+ * Class ArticleFixture
+ * @package AppBundle\Entity
+ *
+ * Génère des valeurs random d'Articles via DoctrineFixturesBundle
+ */
+class UsersFixtures extends Fixture
+{
+
+
+    /**
+     * Load data fixtures with the passed EntityManager
+     *
+     * @param ObjectManager $manager
+     */
+    public function load(ObjectManager $manager)
+    {
+        // create 20 users
+        for ($i = 0; $i < 5; $i++) {
+            $user = new User();
+
+            $user->setEmail("user".$i."@example.com");
+            $user->setUsername("user".$i);
+            $user->setPlainPassword($i);
+            $manager->persist($user);
+            $user->setEnabled(true);
+        }
+
+        $admin = new User();
+        $admin->setUsername("admin");
+        $admin->setPlainPassword("admin");
+        $admin->setEmail("admin@example.com");
+        $admin->setRoles([User::ROLE_SUPER_ADMIN]);
+        $admin->setEnabled(true);
+        $manager->persist($admin);
+
+
+
+        $manager->flush();
+    }
+}
